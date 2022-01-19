59687665af6446a3fb1f6ab946f00a08f39b2860
bharathikarthik2010 <72929446+bharathikarthik2010@users.noreply.github.com>
2022-01-19 15:13:39 +1100

    Add files via upload

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,6 @@
-import boto3
+
 import json
 
-ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
+    response = "Lambda Function 1 !"
     return {"statusCode": 200, "body": json.dumps(response)}
