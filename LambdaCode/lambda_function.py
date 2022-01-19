f4cda70bc18227534d1203c2f6a1e7d9f361726e
LAPTOP-8MBMUK7O\RAX_KD <bharathi.venu.22@gmail.com>
2022-01-20 00:05:16 +1100

    Change5

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -2,5 +2,5 @@
 import json
 
 def lambda_handler(event, context):
-    response = "Lambda Function 1 !"
+    response = "Lambda Function 2 - new  !"
     return {"statusCode": 200, "body": json.dumps(response)}
