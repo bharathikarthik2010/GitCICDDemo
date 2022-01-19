fd2200727a16cee4626e25e1ce6a50de962c08df
LAPTOP-8MBMUK7O\RAX_KD <bharathi.venu.22@gmail.com>
2022-01-20 09:47:27 +1100

    Change Demo1

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -2,5 +2,5 @@
 import json
 
 def lambda_handler(event, context):
-    response = "Lambda Function 2 - new  !"
+    response = "Code Pipeline Demo - Test 1 !"
     return {"statusCode": 200, "body": json.dumps(response)}
