cf62a9996179ae74ef9114b827f2ac118ddef906
LAPTOP-8MBMUK7O\RAX_KD <bharathi.venu.22@gmail.com>
2022-01-28 13:36:43 +1100

    Test many function

--- LambdaCode/TestFunction.py
+++ LambdaCode/TestFunction.py
@@ -1,5 +1,9 @@
 import json
 
 def lambda_handler(event, context):
-    response = "Sam - Lambda function called !"
+    response = "Sam - Lambda function - lambda_handler called !"
     return {"statusCode": 200, "body": json.dumps(response)}
+
+def Show_handler(event, context):
+    response = "Sam - Lambda function - Show_handler called !"
+    return {"statusCode": 200, "body": json.dumps(response)}    
