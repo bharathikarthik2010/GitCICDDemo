59ac962391a26bcd072b26ee0076d6f8046d715f
LAPTOP-8MBMUK7O\RAX_KD <bharathi.venu.22@gmail.com>
2022-01-28 13:28:37 +1100

    Test deployment

--- LambdaCode/TestFunction.py
+++ LambdaCode/TestFunction.py
@@ -1,5 +1,5 @@
 import json
 
 def lambda_handler(event, context):
-    response = "Code Pipeline Demo - Test 1234 !"
+    response = "Sam - Lambda function called !"
     return {"statusCode": 200, "body": json.dumps(response)}
