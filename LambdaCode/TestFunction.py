190fd07972b69e2c10beb8f7dd06b93c37368252
LAPTOP-8MBMUK7O\RAX_KD <bharathi.venu.22@gmail.com>
2022-01-28 14:42:18 +1100

    Demo Commit1

--- LambdaCode/TestFunction.py
+++ LambdaCode/TestFunction.py
@@ -1,9 +1,9 @@
 import json
 
 def lambda_handler(event, context):
-    response = "Sam - Lambda function - lambda_handler called !"
+    response = "Change for Demo Sam - Lambda function - lambda_handler called !"
     return {"statusCode": 200, "body": json.dumps(response)}
 
 def Show_handler(event, context):
-    response = "Sam - Lambda function - Show_handler called !"
+    response = "Change for demo Sam - Lambda function - Show_handler called !"
     return {"statusCode": 200, "body": json.dumps(response)}    
